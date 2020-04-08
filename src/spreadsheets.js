98eba4f3506ade95f378de1f8a7873ea2ba0bfc3
Philip Tchernavskij <ptchernavskij@protonmail.com>
2020-04-08 00:31:53 -0400

    Cleanup and add TODOs

--- src/spreadsheets.js
+++ src/spreadsheets.js
@@ -18,6 +18,12 @@ module.exports = spreadsheets;
 /*
  * for more realistic components where several reads are made over the component's life cycle,
  * we might want to have sub-components define each "sub-session" relating to a new spreadsheetId
+ * 
+ * TODO: make a grade spreadsheets.spreadsheet that maps an entire spreadsheet
+ * It should probably be something like
+ * model: {
+ *   sheet1: [['Name', 'Age', 'Hobby'], ['Philip', 27, 'Role-playing games'], ['Colin', '??', 'Beer brewing']]
+ * }
  */ 
 
 // component for reading a single cell once
@@ -37,9 +43,10 @@ fluid.defaults('spreadsheets.cell', {
    listeners: {
        'onCreate.getCellContent': {
            func: 'spreadsheets.cell.getCellContent',
-           args: ['{that}',
-                  '{session}']
+           args: ['{that}', '{session}']
        },
+       // TODO: ask A about oldstyle event passing versus current setup, 
+       // which is an onCreate invoker
        // '{that}.session.events.onResolve': 'spreadsheets.cell.updateValue',
        // '{that}.session.events.onError': 'spreadsheets.cell.displayError'
    }
@@ -56,31 +63,13 @@ spreadsheets.cell.getCellContent = function (that, session) {
     });
 };
 
-/**
- * Update the cell value with the returned value from a read.
- * @see https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/get
- * @param {Any} value The returned value
- */
-spreadsheets.cell.updateValue = function(value) {
-    // TODO:
-    console.log('Received value from the remote cell: ', value);
-};
-
-/**
- * Display an error in the cell data fetching process
- * @param {Error} error The error produced during the fetch.
- */
-spreadsheets.cell.displayError = function(error) {
-    // TODO: use console.log to begin with
-    console.log('Received an error while trying to read the remote cell: ', error);
-};
-
 fluid.defaults('spreadsheets.clientSession', {
     gradeNames: ['fluid.component'],
     members: {
         sheetsClient: null, // an authorized google API client object, filled with onCreate.impl
     },
     // TODO: ask A whether refactoring to a listener is preferrable here
+    // as in a few lines down
     invokers: {
         get: 'spreadsheets.clientSession.get' // returns a promise
     },
@@ -89,14 +78,6 @@ fluid.defaults('spreadsheets.clientSession', {
         // 'onRead.impl': 'spreadsheets.clientSession.doRead',
     }
 });
-
-spreadsheets.clientSession.get = function(that, spreadsheetId, sheetName, coordinate) {
-    let promiseTogo = that.sheetsClient.spreadsheets.values.get({
-        spreadsheetId, 
-        range: sheetName + '!' + coordinate
-    });
-    return promiseTogo;
-}
 
 spreadsheets.clientSession.doCreate = function(that) {
     var credentials = fs.readFileSync(CREDENTIALS_PATH);
@@ -120,132 +101,19 @@ spreadsheets.clientSession.doCreate = function(that) {
     });
 };
 
-/*
-spreadsheets.clientSession.doCreate = function(that) {
-    console.log('attempting to create clientSession');
-    console.log(that);
-    // TODO:
-    // get and parse credentials from file 
-    fs.readFile(CREDENTIALS_PATH).then(function(content) {
-        var credentials = JSON.parse(content);
-        var { client_secret, client_id, redirect_uris } = credentials.installed;
-        var oAuth2Client = new google.auth.OAuth2(
-            client_id, client_secret, redirect_uris[0]);
-        
-        fs.readFile(TOKEN_PATH).then(function(token) {
-            console.log('read token file successfully');
-            oAuth2Client.setCredentials(JSON.parse(token));
-        }, function(err) {
-            console.log('creating new token');
-            var authUrl = oAuth2Client.generateAuthUrl({
-                access_type: 'offline',
-                scope: SCOPES
-            });
-
-            // TODO: how do I externalize this interaction?
-            console.log('Authorize this app by visiting this url: ' + authUrl);
-            const rl = readline.createInterface({
-                input: process.stdin,
-                output: process.stdout
-            });
-            rl.question('Enter the code from that page here: ', function (code) {
-                rl.close();
-                oAuth2Client.getToken(code, function (err, token) {
-                    if (err) {
-                        that.events.onError.fire(err);
-                    } else {
-                        oAuth2Client.setCredentials(token);
-                        // store the token to disk 
-                        fs.writeFile(TOKEN_PATH, JSON.stringify(token), function (err) {
-                            if (err) {
-                                that.events.onError.fire(err);
-                            }
-                        })
-                    }
-                });
-            });
-        });
-
-        // FIXME: we never get here because I don't understand promises
-        // do I need to have this stuff happen in a then?
-        console.log('creating sheets client');
-        that.sheetsClient = new google.sheets({
-            version: 'v4',
-            auth: oAuth2Client
-        });
-    }).catch(function (err) {
-        console.log(err);
-        that.events.onError.fire(err);
-    });
-};
-*/
-
-
-/* For executing the client read, I wonder what is most faithful to the REST request idiom
- * options:
- * send search specification as
- *   1. parameter list
- *   2. parameter object (can Infusion resolve IoC refs in an object given as args?)
- *   3. members on the session
- * I think parameter object is most faithful to the notion of a request body
- * for now I can just do an array
- */
-
 /**
  * Executes a call to spreadsheets.values.get
  * @see https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/get
- * @param {String} spreadSheetId the id of the host spreadsheet, which can be extracted from its URL
- * @param {String} sheetName the name of the host sheet, defaults to "Sheet1" in single-sheet spreadsheets
- * @param {String} range the range of values to get, expressed in A1 notation
- * @fires spreadsheets.clientSession.onResolve
- * @fires spreadsheets.clientSession.onError
+ * @param {Object} that the clientSession component.
+ * @param {String} spreadSheetId the id of the host spreadsheet, which can be extracted from its URL.
+ * @param {String} sheetName the name of the host sheet, defaults to "Sheet1" in single-sheet spreadsheets.
+ * @param {String} range the range of values to get, expressed in A1 notation.
+ * @return {Promise} resolves with the addressed value or a rejection.
  */
-/*
-spreadsheets.clientSession.doRead = async function(spreadsheetId, sheetName, range) {
-    // TODO:
-    // the parsed result is a 2D array, 
-    // so for a single value it's [[value]]
-    try {
-        that.sheetsClient.values.get({ spreadsheetId, sheetName, range}).then(function(result) {
-            //do stuff with result
-        })
-        var result = await that.sheetsClient.values.get({ spreadSheetId, sheetName, range });
-        result = JSON.parse(result);
-        console.log('Got result! ', result);
-        that.events.onResolve.fire(result);
-    } catch (error) {
-        that.events.onError.fire(error);
-    }
-};
-*/
-
-// based on the range, gives a properly dimensioned model
-// actually the component ought to be specified so it can contain
-// a number of child queries into the same spreadsheet,
-// whether ranges or single cells
-// the point of this is that it gets to reuse the authorized client,
-// and it implies a flow where specifying the sheet and specifying where your attention
-// is in it are separate steps, and one of them happens more often than the other.
-/* TODO:
-fluid.defaults('spreadsheets.sheetRange', {
-
-});
-*/
-
-// does a paginated resource extend downwards, keeping everything in memory,
-// or does it remove the previous material, assuming it's on the client to
-// garbage collect or memorize that state?
-// could I adapt a grade written one way into one written the other way,
-// in both directions?
-/* TODO:
-fluid.defaults('spreadsheets.paginatedSheet', {
-    events: {
-        nextPage: null,
-        previousPage: null
-    },
-    listeners: {
-       'nextPage.loadNextPage': "",
-       'previousPage.loadPreviousPage': ""
-    }
-})
-*/
+spreadsheets.clientSession.get = function(that, spreadsheetId, sheetName, coordinate) {
+    let promiseTogo = that.sheetsClient.spreadsheets.values.get({
+        spreadsheetId, 
+        range: sheetName + '!' + coordinate
+    });
+    return promiseTogo;
+}
